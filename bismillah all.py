a66bac6d276c98d53cecdab2eaa54dcafbec3530
bloomflwrn <irenaflorencia980@gmail.com>
2025-06-23 13:55:13 +0700

    bismillah all.py

--- bismillah all.py
+++ bismillah all.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
+import seaborn as 
 
-st.title("Halo dari GitHub!")
-st.write("Aplikasi Streamlit ini berjalan langsung dari repo GitHub.")
+#option menu di sidebar
+with st.sidebar:
+  selected = option_menu(
+    menu_title = "Tugas Akhir",
+    options = ["Preprocessing", "Segmentasi", "Klasifikasi"],
+    menu_icon = None,
+    icons = ["house", "hr", "diagram-3, "person"],
+    default_index = 0,
+    style = {
+      "icon" : {"color" : "blue"},
+      "nav-link" : {
+        "text-align:" : "left",
+        "margin" : "0px",
+        "--hover-color" : "#eee"
+      }
+    }
+  )
